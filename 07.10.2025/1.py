6d16d2d05c4f249bdda5de01b19fcdddc894888f
Slav Slavov <slavslavov10@yahoo.com>
2025-10-07 18:30:49 +0300

    Add Python scripts for geometric and tax calculations.

---
+++ 07.10.2025/1.py
@@ -0,0 +1,7 @@
+a = float(input('a: '))
+b = float(input('b: '))
+h = float(input('h: '))
+
+area = ((a + b) * h /2)
+
+print(f"Area: {area:.2f}")
---
+++ 07.10.2025/3.py
@@ -0,0 +1,6 @@
+hours = float(input('Hours: '))
+tariff = float(input('Tariff: '))
+
+tax = hours * tariff
+
+print(f'Tax: {tax:.2f}')
