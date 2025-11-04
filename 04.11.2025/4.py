9cda1a5b1c89f28196a1628e8d547b236ca13df7
Slav Slavov <slavslavov10@yahoo.com>
2025-11-04 18:49:05 +0200

    Update `replace_smaller_numbers_with_zero` to return `None` for clarity and consistency

--- 04.11.2025/4.py
+++ 04.11.2025/4.py
@@ -1,9 +1,8 @@
-def replace_smaller_numbers_with_zero(numbers: list[int], number: int) -> list[int]:
+def replace_smaller_numbers_with_zero(numbers: list[int], number: int) -> None:
 	if not numbers:
 		return []
 
 	for i, num in enumerate(numbers):
 		if num > number:
 			numbers[i] = 0
-
-	return numbers
+	return None
